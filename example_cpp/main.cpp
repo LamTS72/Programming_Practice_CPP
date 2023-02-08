154a9f2dc6b3ba2a08d5822a4da1e6116a298198
LamTS72 <sonlam7220@gmail.com>
2023-02-08 07:26:25 +0700

    Initial Commit

---
+++ example_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  example_cpp
+//
+//  Created by Chessman on 08/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
