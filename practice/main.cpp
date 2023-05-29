ad809b9bbbbcd0dae3cb2cd2bd9a1c85e73d1d63
LamTS72 <sonlam7220@gmail.com>
2023-05-29 14:23:15 +0700

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Chessman on 29/05/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
